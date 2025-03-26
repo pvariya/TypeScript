4eeafc5d71fb7abbe4b8a4efcdcf36d30b500a40
pvariya <variyapurv4211@gmail.com>
2025-03-26 09:02:02 -0700

    basic types

--- Basic Type/app.js
+++ Basic Type/app.js
@@ -9,4 +9,35 @@
 // mention type at the time of declaration
 let arr = ["purv", 1];
 let arr2 = [1, "purv"];
+// ---------------------
 // Enums
+var userRole;
+(function (userRole) {
+    userRole["ADMIN"] = "admin";
+    userRole["USER"] = "guest";
+    userRole["AUTHOR"] = "author";
+})(userRole || (userRole = {}));
+//write userRole and give suggestions
+userRole.ADMIN;
+// -----------------------
+// Any
+let anyType; // USE save any type of data
+let any;
+//Unknown
+let a;
+// Void
+// function not return value so this function is void
+function print() {
+    console.log("Hello");
+}
+//null
+let n;
+//undefined
+let u;
+//never
+// never rarely use  
+// function never return value 
+function error(message) {
+    throw new Error(message);
+}
+// function that never ends
--- Basic Type/app.ts
+++ Basic Type/app.ts
@@ -5,14 +5,46 @@
 // - Enum
 // - Any,Unknown,Void,Null, Undefined, Never
 
-
-
-
 // Tuples
 // mention type at the time of declaration
 let arr: [string, number] = ["purv", 1];
 let arr2: [number, string] = [1, "purv"];
-
-
+// ---------------------
 
 // Enums
+enum userRole {
+  ADMIN = "admin",
+  USER = "guest",
+  AUTHOR = "author",
+}
+//write userRole and give suggestions
+userRole.ADMIN;
+// -----------------------
+
+// Any
+let anyType: any; // USE save any type of data
+let any;
+
+//Unknown
+let a: unknown;
+
+// Void
+// function not return value so this function is void
+function print(): void {
+  console.log("Hello");
+}
+
+//null
+let n: null;
+
+//undefined
+let u: undefined;
+
+
+//never
+// never rarely use  
+// function never return value 
+function error(message: string): never {
+  throw new Error(message);
+}
+// function that never ends
