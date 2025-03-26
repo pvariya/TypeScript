c3b86958de1d4b9b471b97ec627dce8574df92ee
pvariya <variyapurv4211@gmail.com>
2025-03-26 08:44:54 -0700

    Tuples

---
+++ Basic Type/app.js
@@ -0,0 +1,12 @@
+"use strict";
+// Basic Type
+// - Primitive types (numbers , strings, booleans)
+// - arrays
+// - Tuple
+// - Enum
+// - Any,Unknown,Void,Null, Undefined, Never
+// Tuples
+// mention type at the time of declaration
+let arr = ["purv", 1];
+let arr2 = [1, "purv"];
+// Enums
---
+++ Basic Type/app.ts
@@ -0,0 +1,18 @@
+// Basic Type
+// - Primitive types (numbers , strings, booleans)
+// - arrays
+// - Tuple
+// - Enum
+// - Any,Unknown,Void,Null, Undefined, Never
+
+
+
+
+// Tuples
+// mention type at the time of declaration
+let arr: [string, number] = ["purv", 1];
+let arr2: [number, string] = [1, "purv"];
+
+
+
+// Enums
